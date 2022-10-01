462c78ff455b84e76d847c698c03703c6790152f
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2022-10-01 13:57:51 -0400

    fix errors and add server and request count info

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var SERVER_NAME = 'Yachao_Assignment_1';
 var PORT = 5000;
 var HOST = '127.0.0.1';
+var colors = require('colors');
+var getCount = 0;
+var postCount = 0;
 
 var restify = require('restify')
 
@@ -10,8 +13,13 @@ var restify = require('restify')
   // Create the restify server
   , server = restify.createServer({ name: SERVER_NAME})
 
-  server.listen(PORT, HOST, function () {
-  console.log('Server %s listening at %s', server.name, server.url)
+server.listen(PORT, HOST, function () {
+
+    console.log('***************************************'.yellow)
+    console.log('server name: '.green, server.name.cyan);
+    console.log('Server listening at: '.green, server.url.cyan)
+    console.log('Endpoints: '.green, (server.url + '/images').underline.blue + ' ' +'Methods: GET POST DELETE'.magenta)
+    console.log('***************************************'.yellow)
 })
 
 server
@@ -24,25 +32,29 @@ server
 
    // Get all images in the system
  server.get('/images', function (req, res, next) {
-  
-  console.log("Images GET : Received request to get all images"); 
+   getCount++;
+   console.log("\n")
+  console.log("processed Request Count -->" + "GET: " + getCount.toString().blue + " POST: " + postCount.toString().blue);
+  console.log("Images GET :" + " Received request to get all images".green); 
   // Find every entity within the given collection
   imagesSave.find({}, function (error, images) {
 
     // Return all of the users in the system
-    console.log("Images GET : Returning all images");
-    res.send(images)
+    console.log("Images GET :" + " Returning all images".green);
+    console.log("\n")
+    res.send(images);
   })
 })
 
 
 // Create a new image
 server.post('/images', function (req, res, next) {
-  
+  postCount++;
+  console.log("\n")
+  console.log("processed Request Count -->" + "GET: " + getCount.toString().blue + " POST: " + postCount.toString().blue);
+  console.log("images POST: " + "Received request to create a new image".green);
 
-  console.log("images POST: Received request to create a new image");
-
-  // Make sure name is defined
+  // Make sure all the required fields are present
   if (req.body.name === undefined ) {
     // If there are any errors, pass them to next in the correct format
     return next(new restify.InvalidArgumentError('name must be supplied'))
@@ -51,13 +63,11 @@ server.post('/images', function (req, res, next) {
     // If there are any errors, pass them to next in the correct format
     return next(new restify.InvalidArgumentError('url must be supplied'))
   }
-
-
   if (req.body.size === undefined ) {
     // If there are any errors, pass them to next in the correct format
     return next(new restify.InvalidArgumentError('size must be supplied'))
   }
-
+ 
   const newImage = {
     name: req.body.name,
     url: req.body.url,
@@ -70,7 +80,8 @@ server.post('/images', function (req, res, next) {
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
    
-    console.log("images POST: Created new image with name " + image.name);
+    console.log("images POST: Created new image with name ".green + image.name.cyan);
+    console.log("\n")
     // Send the user if no issues
     res.send(201, image)
   })
@@ -79,15 +90,16 @@ server.post('/images', function (req, res, next) {
 
 // Delete all images
 server.del('/images', function (req, res, next) {
-  
-  console.log("images DELETE: Received request to delete all images");
+  console.log("\n")
+  console.log("images DELETE: " + "Received request to delete all images".red);
   // Delete all images with the persistence engine
   imagesSave.deleteMany({}, function (error, images) {
 
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
   
-    console.log("images DELETE: Deleted all images");
+    console.log("images DELETE: " + "Deleted all images".green);
+    console.log("\n")
     // Send a 200 OK response
     res.send(200,"All images deleted")
   })
