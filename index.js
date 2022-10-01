d1a2f381d4af39e025214202e6620ef7d9367847
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2022-10-01 12:17:59 -0400

    added request handling

--- index.js
+++ index.js
@@ -20,3 +20,75 @@ server
 
   // Maps req.body to req.params so there is no switching between them
   .use(restify.bodyParser())
+
+
+   // Get all images in the system
+ server.get('/images', function (req, res, next) {
+  
+  console.log("Images GET : Received request to get all images"); 
+  // Find every entity within the given collection
+  imagesSave.find({}, function (error, images) {
+
+    // Return all of the users in the system
+    console.log("Images GET : Returning all images");
+    res.send(images)
+  })
+})
+
+
+// Create a new image
+server.post('/images', function (req, res, next) {
+  
+
+  console.log("images POST: Received request to create a new image");
+
+  // Make sure name is defined
+  if (req.body.name === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('name must be supplied'))
+  }
+  if (req.body.url === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('url must be supplied'))
+  }
+
+
+  if (req.body.size === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('size must be supplied'))
+  }
+
+  const newImage = {
+    name: req.body.name,
+    url: req.body.url,
+    size: req.body.size,
+	}
+
+  // Create the user using the persistence engine
+  imagesSave.create( newImage , function (error, image) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+   
+    console.log("images POST: Created new image with name " + image.name);
+    // Send the user if no issues
+    res.send(201, image)
+  })
+})
+
+
+// Delete all images
+server.del('/images', function (req, res, next) {
+  
+  console.log("images DELETE: Received request to delete all images");
+  // Delete all images with the persistence engine
+  imagesSave.deleteMany({}, function (error, images) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+  
+    console.log("images DELETE: Deleted all images");
+    // Send a 200 OK response
+    res.send(200,"All images deleted")
+  })
+})
